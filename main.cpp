30ec538b0c79eb406a6aa78346a4d6caa9976c3b
richelbilderbeek <richel@richelbilderbeek.nl>
2017-06-09 14:24:15 +0200

    Added ChaiScript

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Weffc++"
+#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
+#include <chaiscript/chaiscript.hpp>
+#pragma GCC diagnostic pop
+
 // C++14
 auto f() noexcept 
 {
   return "Hello world\n";
 }
 
-int main() 
+
+double g(const double x)
 {
-  std::cout << f() << '\n';
+  return std::sin(x);
+}
+
+int main()
+{
+  chaiscript::ChaiScript chai;
+  chai.add(chaiscript::fun(&g), "g");
+  double d = chai.eval<double>("g(0.5);");
+  std::cout << d << '\n' << f() << '\n';
 }
